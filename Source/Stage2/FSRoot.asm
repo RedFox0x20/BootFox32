11e7cc13facb79be5fbf59ceaeb2b4ce2b3cd8cb
Tom (RedFox0x20) <tomwest0209@gmail.com>
2021-03-24 14:35:25 +0000

    Root
    Updated root to remove the fixed the KERNEL.BIN entry as this is no-longer required thanks to the update in FileFox32's FSModifier

--- Source/Stage2/FSRoot.asm
+++ Source/Stage2/FSRoot.asm
@@ -11,12 +11,6 @@ times 512 - ($ - $$) db 0	; Fill the rest of the sector with zeroes
 ; Possible 34 files per directory sector
 ; Zero based sector readings
 
-; TEST FILE
-db 0b00001110
-db "KERNEL.BIN"
-dw 18
-dw 50
-
-times 512 - 15 - 2 db 0	; Fill bytes for the FS Root
+times 512 - 2 db 0	; Fill bytes for the FS Root
 
 dw 0 ; Next sector for directory
